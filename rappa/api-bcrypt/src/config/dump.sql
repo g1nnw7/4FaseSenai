9e872885596bb99d697bdf0e4feb6bac1a72f4c8
g1nnw7 <numseiquenomecoloca@gmail.com>
2026-04-09 21:14:12 -0300

    update

---
+++ rappa/api-bcrypt/src/config/dump.sql
@@ -0,0 +1,17 @@
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    sku VARCHAR(50) UNIQUE NOT NULL,
+    status VARCHAR(20) DEFAULT 'Ativo', -- 'Ativo', 'Rascunho', 'Esgotado'
+    estoque INT DEFAULT 0,
+    preco DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    role VARCHAR(50) DEFAULT 'USER'
+);
